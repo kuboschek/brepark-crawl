48e5fbc8fc3fef1217aeece44541dde06d8f0750
bklw <x32000@gmail.com>
2018-12-20 12:42:41 +0100

    Initial working version

---
+++ archive.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import datetime
+import subprocess
+import shutil
+
+ISO_FORMAT = "%Y-%m-%dT%H:%M:%S"
+ISO_DATE = "%Y-%m-%d"
+OUTPUT_DIR = os.getenv('OUTPUT_DIR', './data')
+ARCHIVE_DIR = os.getenv('ARCHIVE_DIR', os.path.join(OUTPUT_DIR, 'archive'))
+TAR_CMD = 'tar -czf {}.tgz {}'
+
+output_dir = os.path.abspath(OUTPUT_DIR)
+archive_dir = os.path.abspath(ARCHIVE_DIR)
+date_today = datetime.datetime.now().strftime(ISO_DATE)
+
+try:
+    os.mkdir(archive_dir)
+except FileExistsError:
+    pass
+
+with os.scandir(OUTPUT_DIR) as it:
+    for obj in it:
+        # Today's folder is still being added to, so we don't tarball it.
+        if obj.is_dir() and obj.name != date_today and obj.name != os.path.basename(ARCHIVE_DIR):
+            cmd = TAR_CMD.format(os.path.join(ARCHIVE_DIR, obj.name), obj.path)
+            print(cmd)
+            ret = subprocess.call(cmd.split(' '))
+
+            if ret != 0:
+                print("Tar returned with errors. Archival interrupted.")
+                exit(1)
+            else:
+                shutil.rmtree(obj.path)
+
+        
---
+++ crawl.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+"""
+    Scrapes the Bremen parking garage page for free spots data.
+    Saves raw response to .json files. One folder per day.
+    Reports errors from HTTP, and if update timestamps are too old.
+"""
+
+import os
+import requests
+import datetime
+
+BREPARK_URL = "https://www.brepark.de/fileadmin/freespaces.json"
+TS_FORMAT = "%a %b %d %H:%M:%S %Y"
+ISO_FORMAT = "%Y-%m-%dT%H-%M-%S"
+ISO_DATE = "%Y-%m-%d"
+OUTPUT_DIR = os.getenv('OUTPUT_DIR', './data')
+
+base_dir = os.path.abspath(OUTPUT_DIR)
+
+# Create the output directories
+try:
+    os.mkdir(base_dir)
+except FileExistsError:
+    pass
+
+r = requests.get(BREPARK_URL)
+now = datetime.datetime.now()
+
+today_dir = os.path.join(base_dir, now.strftime(ISO_DATE))
+
+try:
+    os.mkdir(today_dir)
+except FileExistsError:
+    pass
+
+j = r.json()
+
+# Extract timestamps
+last_update = datetime.datetime.strptime(j['lastUpdate'], TS_FORMAT)
+server_time = datetime.datetime.strptime(j['serverTime'], TS_FORMAT)
+
+# Save crawled file
+with open(os.path.join(today_dir, now.strftime(ISO_FORMAT) + '.json'), 'w') as f:
+    f.write(r.text)
+
+if r.status_code != 200:
+    print(json.dumps({'ok': False, 'reason': r.reason, 'status': r.status_code, 'text': r.text}))
+    exit(1)
+
+if len(r.text) < 512:
+    print(json.dumps({'ok': False, 'reason': 'Response Too Short', 'text': r.text}))
+    exit(2)
+
+# Also fail if the last update is way too old
+if last_update - now > datetime.timedelta(minutes=2):
+    print(json.dumps({'ok': False, 'reason': 'Last Update Too Old', 'text': r.text}))
+    exit(3)
+
+if server_time - now > datetime.timedelta(seconds=10):
+    print(json.dumps({'ok': False, 'reason': 'Server Time Diverged', 'text': r.text}))
+    exit(4)
